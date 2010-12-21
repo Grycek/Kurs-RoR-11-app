4705e73b0a2b73f30db19721be8dc594142ba385
Grycek <adam.grycner@wp.pl>
2010-12-21 11:46:51 +0100

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TmpApp::Application.config.secret_token = 'cdb7bb2e098bb27e82764362a670f624360692df5526d15c596270ff11df17ad0796ecaea3fd641a44a0599a4e3ad9c51157778c397b3c1cd6c1245e89c772e9'
